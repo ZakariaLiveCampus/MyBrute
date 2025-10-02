a5758785ecba5af07d620cf0f385b6823917f4d3
ZakariaLiveCampus <zakaria.saber@livecampus.me>
2025-10-02 15:32:56 +0200

    Ajout de nouveaux assets pour l'opposant

--- src/components/CombatPhaser.jsx
+++ src/components/CombatPhaser.jsx
@@ -21,7 +21,7 @@ const SPRITE_CONFIG = {
   attackDistance: 100,
 };
 
-const ANIMATION_CONFIG = [
+const BRUTE_ANIMATION_CONFIG = [
   {
     key: ANIMATIONS.RUN,
     file: "Biker_run.png",
@@ -90,6 +90,87 @@ const ANIMATION_CONFIG = [
   {
     key: ANIMATIONS.VICTORY,
     file: "Biker_climb.png",
+    frames: 6,
+    frameRate: 10,
+    repeat: -1,
+    frameConfig: {
+      frameWidth: 48,
+      frameHeight: 48,
+      startFrame: 0,
+      endFrame: 5,
+    },
+  },
+];
+
+const OPPONENT_ANIMATION_CONFIG = [
+  {
+    key: ANIMATIONS.RUN,
+    file: "Cyborg_run.png",
+    frames: 6,
+    frameRate: 10,
+    repeat: -1,
+    frameConfig: {
+      frameWidth: 48,
+      frameHeight: 48,
+      startFrame: 0,
+      endFrame: 5,
+    },
+  },
+  {
+    key: ANIMATIONS.IDLE,
+    file: "Cyborg_idle.png",
+    frames: 4,
+    frameRate: 8,
+    repeat: -1,
+    frameConfig: {
+      frameWidth: 48,
+      frameHeight: 48,
+      startFrame: 0,
+      endFrame: 3,
+    },
+  },
+  {
+    key: ANIMATIONS.ATTACK,
+    file: "Cyborg_attack1.png",
+    frames: 6,
+    frameRate: 12,
+    repeat: 0,
+    frameConfig: {
+      frameWidth: 48,
+      frameHeight: 48,
+      startFrame: 0,
+      endFrame: 5,
+    },
+  },
+  {
+    key: ANIMATIONS.HURT,
+    file: "Cyborg_hurt.png",
+    frames: 2,
+    frameRate: 8,
+    repeat: 0,
+    frameConfig: {
+      frameWidth: 48,
+      frameHeight: 48,
+      startFrame: 0,
+      endFrame: 1,
+    },
+  },
+  {
+    key: ANIMATIONS.DEATH,
+    file: "Cyborg_death.png",
+    frames: 6,
+    frameRate: 10,
+    repeat: 0,
+    frameConfig: {
+      frameWidth: 48,
+      frameHeight: 48,
+      startFrame: 0,
+      endFrame: 5,
+    },
+  },
+  {
+    key: ANIMATIONS.VICTORY,
+    file: "Cyborg_climb.png",
     frames: 6,
     frameRate: 10,
     repeat: -1,
@@ -212,14 +293,24 @@ export default function CombatPhaser({
         key: "CombatScene",
         preload: function () {
           this.load.image("arena_bg", "/assets/arena_bg.png");
-          ANIMATION_CONFIG.forEach((anim) => {
-            ["brute", "opponent"].forEach((prefix) => {
-              const key = `${prefix}_${anim.key}`;
-              const file = `/assets/${anim.file}`;
-              this.load.spritesheet(key, file, {
-                frameWidth: SPRITE_CONFIG.frameWidth,
-                frameHeight: SPRITE_CONFIG.frameHeight,
-              });
+
+          // Charger les assets Biker pour la brute
+          BRUTE_ANIMATION_CONFIG.forEach((anim) => {
+            const key = `brute_${anim.key}`;
+            const file = `/assets/${anim.file}`;
+            this.load.spritesheet(key, file, {
+              frameWidth: SPRITE_CONFIG.frameWidth,
+              frameHeight: SPRITE_CONFIG.frameHeight,
+            });
+          });
+
+          // Charger les assets Cyborg pour l'opposant
+          OPPONENT_ANIMATION_CONFIG.forEach((anim) => {
+            const key = `opponent_${anim.key}`;
+            const file = `/assets/${anim.file}`;
+            this.load.spritesheet(key, file, {
+              frameWidth: SPRITE_CONFIG.frameWidth,
+              frameHeight: SPRITE_CONFIG.frameHeight,
             });
           });
         },
@@ -227,26 +318,47 @@ export default function CombatPhaser({
           sceneRef.current = this;
           this.add.image(300, 150, "arena_bg").setDisplaySize(600, 300);
 
-          ["brute", "opponent"].forEach((prefix) => {
-            ANIMATION_CONFIG.forEach((anim) => {
-              const animKey = `${anim.key}_${prefix}`;
-              const spriteKey = `${prefix}_${anim.key}`;
+          // Créer les animations pour la brute (Biker)
+          BRUTE_ANIMATION_CONFIG.forEach((anim) => {
+            const animKey = `${anim.key}_brute`;
+            const spriteKey = `brute_${anim.key}`;
 
-              if (this.anims.exists(animKey)) {
-                this.anims.remove(animKey);
-              }
+            if (this.anims.exists(animKey)) {
+              this.anims.remove(animKey);
+            }
 
-              this.anims.create({
-                key: animKey,
-                frames: this.anims.generateFrameNumbers(spriteKey, {
-                  start: anim.frameConfig.startFrame,
-                  end: anim.frameConfig.endFrame,
-                }),
-                frameRate: anim.frameRate,
-                repeat: anim.repeat,
-                yoyo: anim.key === ANIMATIONS.HURT,
-                hideOnComplete: false,
-              });
+            this.anims.create({
+              key: animKey,
+              frames: this.anims.generateFrameNumbers(spriteKey, {
+                start: anim.frameConfig.startFrame,
+                end: anim.frameConfig.endFrame,
+              }),
+              frameRate: anim.frameRate,
+              repeat: anim.repeat,
+              yoyo: anim.key === ANIMATIONS.HURT,
+              hideOnComplete: false,
+            });
+          });
+
+          // Créer les animations pour l'opposant (Cyborg)
+          OPPONENT_ANIMATION_CONFIG.forEach((anim) => {
+            const animKey = `${anim.key}_opponent`;
+            const spriteKey = `opponent_${anim.key}`;
+
+            if (this.anims.exists(animKey)) {
+              this.anims.remove(animKey);
+            }
+
+            this.anims.create({
+              key: animKey,
+              frames: this.anims.generateFrameNumbers(spriteKey, {
+                start: anim.frameConfig.startFrame,
+                end: anim.frameConfig.endFrame,
+              }),
+              frameRate: anim.frameRate,
+              repeat: anim.repeat,
+              yoyo: anim.key === ANIMATIONS.HURT,
+              hideOnComplete: false,
             });
           });
 
